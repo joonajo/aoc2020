3b12db6250964e81b8bdd6b42f8ef799fd976d44
joonajo <joona.joenpolvi@wolt.com>
2020-12-08 23:05:28 +0200

    Day 4 part 2 solution

--- day4/day4.ts
+++ day4/day4.ts
@@ -2,7 +2,7 @@ import { readInput } from "../common";
 
 const input = readInput('day4/input.txt')
 
-const runPart1 = () => {
+const run = () => {
 
     const data = [...input]
 
@@ -16,8 +16,17 @@ const runPart1 = () => {
 
             const passportData = data.splice(0, nextCutoff).join(', ')
 
-            if (requiredFields.every(field => passportData.includes(field))) {
-                validPassports.push(passportData)
+            if (requiredFields.every(field => passportData.includes(field))) {                
+                const splitPassportData = passportData.split(' ')
+                const passport = {}
+
+                for (let field of requiredFields) {
+                    const fieldData = splitPassportData.find(val => val.includes(field)).split(':')[1]   
+
+                    passport[field] = fieldData.replace(/,/g, "")
+                }
+
+                validPassports.push(passport)
             }
         }
 
@@ -26,6 +35,58 @@ const runPart1 = () => {
 
     const passports = splitToPassports()
     console.log('Part 1 solution: ', passports.length)
+
+    const validPassports = []
+
+    const validEcl = ['amb', 'brn', 'blu', 'gry', 'grn', 'hzl', 'oth']
+
+    for (let passport of passports) {
+        let isValid = true
+
+        const byr = passport.byr as number
+        const iyr = passport.iyr as number
+        const eyr = passport.eyr as number
+        const hcl = passport.hcl as string
+        const ecl = passport.ecl as string
+        const pid = passport.pid as string
+        if (byr < 1920 || byr > 2002) {
+            isValid = false
+        }
+        if (iyr < 2010 || iyr > 2020) {
+            isValid = false
+        }
+        if (eyr < 2020 || eyr > 2030) {
+            isValid = false
+        }
+        if (!hcl.includes('#')) {
+            isValid = false
+        }
+        if (hcl.length !== 7) {
+            isValid = false
+        }
+
+        if (!validEcl.includes(ecl)) {
+            isValid = false
+        }
+
+        if (pid.length !== 9) {
+            isValid = false
+        } 
+
+        const maxHeight = passport.hgt.includes('cm') ? 193 : 76
+        const minHeight = passport.hgt.includes('cm') ? 150 : 59
+        const height = passport.hgt.replace(/\D/g, "")
+
+        if (height < minHeight || height > maxHeight) {
+            isValid = false
+        } 
+
+        if (isValid) {
+            validPassports.push(passport)
+        }
+    }
+
+    console.log('Part 2 solution: ', validPassports.length)
  }
 
-runPart1()
+run()
