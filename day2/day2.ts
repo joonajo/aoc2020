d2a7beba871cc4ab9ff657d48e88fd4d2126a3f5
joonajo <joona.joenpolvi@wolt.com>
2020-12-08 19:50:28 +0200

    Day 2 part 2 solution

--- day2/day2.ts
+++ day2/day2.ts
@@ -25,4 +25,33 @@ const runPart1 = () => {
     console.log('Part 1 solution:', numOfValidPassowords)
 }
 
+const runPart2 = () => {
+    let numOfValidPassowords = 0
+
+    for (const combination of input) {
+        let splitCombination = combination.split('-')
+
+        const [indexOne] = splitCombination as unknown as number[]
+        splitCombination = splitCombination[1].split(' ')
+        const [indexTwo] = splitCombination as unknown as number[]
+        const [passwordLetter] = splitCombination[1].split(':')
+        const passwordCombination = splitCombination[2]
+
+        if (passwordCombination[indexOne - 1] === passwordLetter) {
+            if (passwordCombination[indexTwo - 1] !== passwordLetter) {
+                numOfValidPassowords++
+            }
+        }
+
+        if (passwordCombination[indexOne - 1] !== passwordLetter) {
+            if (passwordCombination[indexTwo - 1] === passwordLetter) {
+                numOfValidPassowords++
+            }
+        }
+    }
+
+    console.log('Part 2 solution:', numOfValidPassowords)
+}
+
 runPart1()
+runPart2()
