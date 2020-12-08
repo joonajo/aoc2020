ab674a01f08d3f9cd89a2ead67d42d9d4d65216f
joonajo <joona.joenpolvi@wolt.com>
2020-12-08 19:11:06 +0200

    Day 1 part 2 solution

--- day1/day1.ts
+++ day1/day1.ts
@@ -6,7 +6,7 @@ const input = readInput('day1/input.txt')
 
 const TARGET_NUMBER = 2020
 
-const run = () => {
+const part1 = () => {
     for (const num of input) {
         const currentNumber = (num as unknown as number)
         const neededNumber = TARGET_NUMBER - currentNumber
@@ -17,4 +17,23 @@ const run = () => {
     }
 }
 
-console.log('Part 1 solution:', run())
+const part2 = () => {
+    for (let i = 0; i < input.length - 1; i++) {
+        for (let j = i + 1; j < input.length - 1; j++) {
+            const numberOne = input[i] as unknown as number
+            const numberTwo = input[j] as unknown as number
+
+            const neededNumber = TARGET_NUMBER - numberOne - numberTwo
+
+            if (neededNumber > 0) {
+                if (input.includes(neededNumber.toString())) {
+                    return numberOne * numberTwo * neededNumber
+                }
+            }
+        }
+    }
+}
+
+
+console.log('Part 1 solution:', part1())
+console.log('Part 2 solution:', part2())
