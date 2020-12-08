5f151e8d028dca03ca7e6a3e5e4d14071ffb7f0d
joonajo <joona.joenpolvi@wolt.com>
2020-12-08 21:18:41 +0200

    Day 3 part 2 solution

--- day3/day3.ts
+++ day3/day3.ts
@@ -11,14 +11,51 @@ const runPart1 = () => {
     let movement = 3
 
     for (const line of input) {
-        
+
         if (line[currPosition] === TREE_SYMBOL) {
             numOfTrees++
         }
         currPosition = (currPosition + movement) < line.length ? currPosition + movement : currPosition + movement - line.length
     }
 
-    console.log(numOfTrees)
+    console.log('Part 1 solution:', numOfTrees)
+}
+
+const runPart2 = () => {
+        
+    const numOfTrees: number[] = []
+    let movements = [
+        { x: 1, y: 1 },
+        { x: 3, y: 1 },
+        { x: 5, y: 1 },
+        { x: 7, y: 1 },
+        { x: 1, y: 2 },
+    ]
+
+    for (let movement of movements) {
+        let numberOfTreesForMovement = 0
+        let currPosition = 0
+        for (let i = 0; i < input.length; i += movement.y) {
+            if (!input[i]) {
+                break
+            }
+
+            const line = input[i]
+
+            if (line[currPosition] === TREE_SYMBOL) {
+                numberOfTreesForMovement++
+            }
+
+            currPosition = (currPosition + movement.x) < line.length ? currPosition + movement.x : currPosition + movement.x - line.length
+        }
+
+        numOfTrees.push(numberOfTreesForMovement)
+    }
+
+    const solution = numOfTrees.reduce((current, value) => current * value, 1)
+    
+    console.log('Part 2 solution:', solution)
 }
 
 runPart1()
+runPart2()
