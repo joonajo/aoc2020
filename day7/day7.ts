f12c11be8728fc129397fbced28a05f5dd4a1db4
joonajo <joona.joenpolvi@wolt.com>
2020-12-09 20:45:59 +0200

    Day 7 part 2 solution

--- day7/day7.ts
+++ day7/day7.ts
@@ -2,7 +2,7 @@ import { readInput } from "../common";
 
 const input = readInput('day7/input.txt')
 
-const TARGET_BAG = 'shiny gold bag'
+const TARGET_BAG = 'shiny gold'
 
 const run = () => {    
     const findBagsThatContainTarget = (target: string) => {
@@ -11,8 +11,8 @@ const run = () => {
         const bagsThatContainTargetBag = []
 
         linesWithTargetBag.forEach(line => {
-            if (!line.split('bags')[0].includes('shiny gold')) {
-                bagsThatContainTargetBag.push(line.split(' bags')[0])
+            if (!line.split('bags')[0].includes(TARGET_BAG)) {
+                bagsThatContainTargetBag.push(line.split(' bag')[0])
             }
         })
         return bagsThatContainTargetBag
@@ -32,6 +32,52 @@ const run = () => {
     }
 
     console.log("Part 1 solution:", checkedBags.length - 1)
+
+    const findBagsThatTargetContains = (target: Bag) => {
+        const lineWithBag = input.find(line => {
+            const splitLine = line.split(' ')
+            return splitLine[0] + ' ' + splitLine[1] === target.bag
+        })
+
+        const splitLine = lineWithBag.split(' ').splice(4,)
+
+        const containedBags = []
+        
+        if (splitLine[0] === 'no') return []
+
+        while (splitLine.length > 0) {
+            containedBags.push({
+                amount: parseInt(splitLine.shift()) * target.amount,
+                bag: splitLine.shift() + ' ' + splitLine.shift()
+            })    
+
+            if (splitLine.length > 0) {
+                splitLine.shift()
+            }
+        }
+
+        return containedBags
+    }
+
+    type Bag = {
+        amount: number;
+        bag: string;
+    }
+
+    const containedBags: Bag[]  = []
+    let sum: number = 0
+
+    containedBags.push({ amount: 1, bag: 'shiny gold' })
+    
+    while (containedBags.length > 0) {
+        const bagToCheck = containedBags.shift()
+        sum += bagToCheck.amount
+        console.log(bagToCheck)
+
+        containedBags.push(...findBagsThatTargetContains(bagToCheck))
+    }
+
+    console.log('Part 2 solution:', sum - 1)
 }
 
 run()
