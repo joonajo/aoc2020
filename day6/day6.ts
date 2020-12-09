5dcce8e5bbd1899a60b732bc52501fa35d3e2ddf
joonajo <joona.joenpolvi@wolt.com>
2020-12-09 18:02:21 +0200

    Day 6 part 2 solution

--- day6/day6.ts
+++ day6/day6.ts
@@ -11,9 +11,13 @@ const run = () => {
     let yesCount = 0
 
     while (data.length > 0) {
-        const nextCutoff = data.indexOf('') !== -1 ? data.indexOf('') + 1 : data.length
+        const nextCutoff = data.indexOf('') !== -1 ? data.indexOf('') : data.length
         const group = data.splice(0, nextCutoff)
         answerGroups.push(group)
+
+        if (!!data.length) {
+            data.shift()
+        }
     }
     
     for (let group of answerGroups) {
@@ -22,6 +26,26 @@ const run = () => {
     }
 
     console.log('Part 1 solution:', yesCount)
+
+    yesCount = 0
+
+    for (let group of answerGroups) {
+        const numOfPeople = group.length
+        const allYesAnswers = group.join('')
+
+        let numOfQuestionsWithAllYes = 0
+
+        for (let question of R.uniq(allYesAnswers)) {
+            const regexp = new RegExp(question, "gi")
+            if ((allYesAnswers.match(regexp) || []).length === numOfPeople) {
+                numOfQuestionsWithAllYes++
+            }
+        }
+
+        yesCount += numOfQuestionsWithAllYes
+    }
+
+    console.log('Part 2 solution:', yesCount)
 }
 
 run()
