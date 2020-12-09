25564d09150d29a1f3871a76c03ae95b4b467af6
joonajo <joona.joenpolvi@wolt.com>
2020-12-09 17:23:00 +0200

    Day 5 part 1 solution

---
+++ day5/day5.ts
@@ -0,0 +1,56 @@
+import { readInput } from "../common"
+
+type Row = 'F' | 'B'
+type Column = 'R' | 'L'
+
+const input = readInput('day5/input.txt')
+
+const takeLowerHalf = (currentRange: { from: number, to: number }) => {
+    const minus = (currentRange.to - currentRange.from) / 2
+    return { ...currentRange, to: currentRange.to - minus }
+}
+
+const takeUpperHalf = (currentRange: { from: number, to: number }) => {
+    const plus = (currentRange.to - currentRange.from) / 2
+    return { ...currentRange, from: currentRange.from + plus }
+}
+
+const ROW_RANGE = { from: 0, to: 127 }
+const COLUMN_RANGE = { from: 0, to: 7 }
+
+const run = () => {
+    let highestSeatId = 0
+    for (const line of input) {
+        let row = ROW_RANGE
+        let column = COLUMN_RANGE
+
+        for (const letter of line) {
+            switch (letter) {
+                case 'F':
+                    row = takeLowerHalf(row)
+                    break;
+                case 'B':
+                    row = takeUpperHalf(row)
+                    break;
+                case 'R':
+                    column = takeUpperHalf(column)
+                    break;
+                case 'L':
+                    column = takeLowerHalf(column)
+                    break;
+                default:
+                    break;
+            }
+        }
+        const finalRow = Math.round((row.from + row.to) / 2)
+        const finalColumn = Math.round((column.from + column.to) / 2)
+        const seatId = finalRow * 8 + finalColumn
+        if (seatId > highestSeatId) {
+            highestSeatId = seatId
+        }
+    }
+
+    console.log('Part 1 solution:', highestSeatId)
+}
+
+run()
