a24f6a3d76c6387cd8d8d7c861b1ffa7586922f4
joonajo <joona.joenpolvi@wolt.com>
2020-12-10 19:27:39 +0200

    Day 9 part 2 solution

--- day9/day9.ts
+++ day9/day9.ts
@@ -1,6 +1,7 @@
 import { readInput } from "../common";
 
 const input = readInput('day9/input.txt')
+const parsedInput = input.map(val => parseInt(val))
 
 const findPair = (range: number[], target: number): number[] | null => {
     for (let num of range) {
@@ -14,10 +15,8 @@ const findPair = (range: number[], target: number): number[] | null => {
     return null
 }
 
-const run = (preambleCount: number) => {
-    const parsedInput = input.map(val => parseInt(val))
-
-    for (let i = preambleCount; i < parsedInput.length ; i++) {
+const findInvalidNumber = (preambleCount: number) => {
+    for (let i = preambleCount; i < parsedInput.length - 1; i++) {
         const target = parsedInput[i]
         const range = parsedInput.slice(i - preambleCount, i)
 
@@ -25,11 +24,33 @@ const run = (preambleCount: number) => {
         
         if (pair === null) {
             console.log('Part 1 solution:', target)
-            return
+            return target
+        }
+    }
+}
+
+const findNumberSet = (target: number) => {
+    for (let i = 0; i < parsedInput.length; i++) {
+        let currentSum = parsedInput[i]
+        for (let j = i + 1; j < parsedInput.length - 1; j++) {
+            const numToCheck = parsedInput[j]
+            currentSum += numToCheck
+            if (currentSum === target) {
+                const range = parsedInput.slice(i, j + 1)
+                const biggestNumber = Math.max(...range)
+                const smallestNumber = Math.min(...range)
+                console.log('Part 2 solution:', biggestNumber + smallestNumber)
+            }            
+
+            if (currentSum > target) {
+                break
+            }
         }
     }
 }
 
 const PREAMBLE_COUNT = 25
 
-run(PREAMBLE_COUNT)
+const invalidNumber = findInvalidNumber(PREAMBLE_COUNT)
+
+findNumberSet(invalidNumber)
