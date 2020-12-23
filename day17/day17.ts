d339333dd7eb94a99383fad66712056e43836837
joonajo <joona.joenpolvi@wolt.com>
2020-12-23 20:12:27 +0200

    WIP: day 17

---
+++ day17/day17.ts
@@ -0,0 +1,15 @@
+import { readInput } from './../common';
+
+const input = readInput('day17/input.txt')
+
+const CYCLE_COUNT = 6
+
+const run = () => {
+    const layers = [[...input]]
+
+    for (let i = 0; i < CYCLE_COUNT; i++) {
+
+    }
+}
+
+run()
